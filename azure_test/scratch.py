f48b93901ea466bbfaa645da6f0c3bbc312bcfc0
komsan <komsan@blendata.co>
2023-11-21 13:37:46 +0700

    save the successful version

--- azure_test/scratch.py
+++ azure_test/scratch.py
@@ -1,4 +1,32 @@
+import json
 import numpy as np
-a = np.array([1, 2, 3])
-y = np.delete(a, [0, 1], axis=1)
-print(y)
+import pickle
+
+def init():
+    global model
+    # Load the model
+    model = pickle.load(open("azure_test/model/model.pkl", "rb"))
+
+def run(raw_data):
+    try:
+        # Convert the raw data to a numpy array
+        data = raw_data["data"]
+        data = np.array(data)
+	
+        # Get the data preprocessed
+        data_prep = preprocess(data).reshape(1, -1)
+        
+        # Make predictions
+        result = model.predict(data_prep)
+
+        # You can return any JSON-serializable result. Here, we return a list.
+        print(json.dumps({"result": result.tolist()}))
+    except Exception as e:
+        error = str(e)
+        print(json.dumps({"error": error}))
+
+def preprocess(data):
+    return np.vectorize(np.float32)(np.delete(data, [2, 3]))
+
+init()
+run({"data": [8937,44551,"2005-12-23",4.0,2,173538]})
